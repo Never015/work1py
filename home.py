e2d2a2561cbd9b05dbaeb7e00848da60207b31b9
Never015 <gun9.aingkarat@gmail.com>
2025-02-25 10:33:50 +0700

    .commit

--- home.py
+++ home.py
@@ -4,5 +4,5 @@ import pandas as pd
 st.title("🐷🐷🐷Website Developing using Python🐷🐷")
 st.header("🍖🍖Website Developing using Python🍖🍖")
 
-st.image('./img/kairung.jpg')
-st.subheader("Kairung Hengpraprohm")
+st.image('./img/21792.jpg')
+st.subheader("Aingkarat boonpleng")
