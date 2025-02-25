d6b33d3bc94451d230d1ade2626d8790ad51c216
Never015 <gun9.aingkarat@gmail.com>
2025-02-25 11:20:09 +0700

    .commit

--- home.py
+++ home.py
@@ -6,3 +6,14 @@ st.header("🍖🍖Website Developing using Python🍖🍖")
 
 st.image('./img/21792.jpg')
 st.subheader("Aingkarat boonpleng")
+col1,col2,col3, = st.columns(3)
+
+with col1 :
+    st.header("skrim")
+    st.image("./img/skyrim.png")
+with col2 :
+    st.header("cat1")
+    st.image("./img/cat.jng")
+with col3 :
+    st.header("bcat")
+    st.image("./img/hq720.jng")
